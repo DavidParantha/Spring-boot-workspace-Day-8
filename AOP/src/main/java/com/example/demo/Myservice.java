27e31666560dbedf847ad69f5a047352340ee387
David <paranthadavid26@gmail.com>
2026-07-03 14:52:01 +0530

    Day 8: Add AOP project

---
+++ AOP/src/main/java/com/example/demo/Myservice.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Myservice {
+
+	public void displayMessage() {
+		System.out.println("Business Method Executed");
+	}
+
+}
